a16d1169e82aece6a58fba0c63ad578a83c7a0f9
baodhtv01 <59009275+baodhtv01@users.noreply.github.com>
2020-10-31 04:35:52 +0700

    Add files via upload

---
+++ js/js1.js
@@ -0,0 +1,57 @@
+//Click on the buttons to open the collapsible content.
+var coll = document.getElementsByClassName("collap");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+//alert
+function subscribeFooter(){
+  alert("The latest news of the BGV Academy will be updated to you \nThanks you!")
+}
+function Mess(){
+  alert("BGV Academy will respond to you as soon as possible \nThanks you!")
+}
+//wellcom
+function myFunction1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+//tablink
+function openPage(evt, pageName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(pageName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+
+
